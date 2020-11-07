543ab6e65e697050142dc5d4eaf30d0719001ec3
Archil Phanchulidze <achiko@gmail.com>
2020-11-07 20:53:19 +0400

    removed ganache

--- contracts/CallActions.sol
+++ contracts/CallActions.sol
@@ -5,6 +5,8 @@ import "./Interfaces/IIERC20.sol";
 import "./Interfaces/IVAULT.sol";
 
 contract CallActions {
+
+    // Vars Used For debugging purpose only , should be removed  
     struct Vars {
         address vaultAddress;
         address collateralToken;
@@ -19,7 +21,8 @@ contract CallActions {
         uint256 userBalanceAfter;
     }
 
-    function callMint(
+    // DelegateCall version 
+    function callMintDelegate(
         address _vaultAddress,
         address _collateralToken,
         uint256 _collateralAmount
@@ -31,9 +34,11 @@ contract CallActions {
         vars.collateralAmount = _collateralAmount;
         vars._sender = msg.sender;
 
-
         vars.userBalance = IIERC20(_collateralToken).balanceOf(msg.sender);
 
+        // 1. Approve Tokens 
+        // Delegate Call : forward origin msg.sender to the conllateral token 
+        // ! DelegateCall cant change collateral token storage
         (bool success, bytes memory data) = 
             _collateralToken.delegatecall(
                     abi.encodeWithSignature("approve(address,uint256)", _vaultAddress, _collateralAmount));
@@ -45,16 +50,39 @@ contract CallActions {
             _vaultAddress
         );
 
-        vars._allowance = IIERC20(_collateralToken).allowance(
-            msg.sender,
-            address(this)
-        );
-        
-
-        vars._break = true;
+        require(vars._allowance != 0, "Allowance == 0");
 
         // 2. Mint
-        //IVAULT(_vaultAddress).mint(_collateralAmount);
+        IVAULT(_vaultAddress).mint(_collateralAmount);
+        success = vars.approveResponse;
+    }
+
+
+    // Standard version 
+    function callMint(
+        address _vaultAddress,
+        address _collateralToken,
+        uint256 _collateralAmount
+    ) public returns (bool success) {
+        
+        IERC20 collateral = IIERC20(_collateralToken);
+
+        // 1. Approve 
+        // !sender is not ms.sender its the contract address address(this)
+        vars.approveResponse = collateral.approve(
+            _vaultAddress,
+            _collateralAmount
+        );
+        
+        vars._allowance = collateral.allowance(
+            msg.sender,
+            _vaultAddress
+        );
+
+        require(vars._allowance != 0, "Allowance == 0");
+
+        // 2. Call mint finction in Valut Contract
+        IVAULT(_vaultAddress).mint(_collateralAmount);
         success = vars.approveResponse;
     }
 }
