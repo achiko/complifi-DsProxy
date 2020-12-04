608f1fe91e9e6734bce9ea56bb7144c38493b1eb
Archil Phanchulidze <achiko@gmail.com>
2020-12-04 18:11:51 +0400

    Tests fixed

--- contracts/CallActions.sol
+++ contracts/CallActions.sol
@@ -39,46 +39,8 @@ contract CallActions {
         vars.primaryTokenBalance = vars.primaryToken.balanceOf(address(this));
         vars.complementTokenBalance = vars.complementToken.balanceOf(address(this));
 
-        // 4. Transfer Primary & Complement tokens to user wallet
+        // 4. Transfer Minted Primary & Complement tokens to user wallet
         require(vars.primaryToken.transfer(msg.sender,vars.primaryTokenBalance), "ERR_TRANSFER_PRIMARY_TOKEN");
         require(vars.complementToken.transfer(msg.sender,vars.complementTokenBalance), "ERR_TRANSFER_COMPLEMENT_TOKEN");
     }
-
-    // To Do Remove :
-    // DelegateCall version 
-    // function callMintDelegate(
-    //     address _vaultAddress,
-    //     address _collateralToken,
-    //     uint256 _collateralAmount
-    // ) public returns (bool success) {
-    //     Vars memory vars;
-
-    //     vars.vaultAddress = _vaultAddress;
-    //     vars.collateralToken = _collateralToken;
-    //     vars.collateralAmount = _collateralAmount;
-    //     vars._sender = msg.sender;
-
-    //     vars.userBalance = IIERC20(_collateralToken).balanceOf(msg.sender);
-
-    //     // 1. Approve Tokens 
-    //     // Delegate Call : forward origin msg.sender to the conllateral token 
-    //     // ! DelegateCall cant change collateral token storage
-    //     (bool success, bytes memory data) = 
-    //         _collateralToken.delegatecall(
-    //                 abi.encodeWithSignature("approve(address,uint256)", _vaultAddress, _collateralAmount));
-
-    //     require(success, "Approve Delegatecall failed !");
-
-    //     vars._allowance = IIERC20(_collateralToken).allowance(
-    //         msg.sender,
-    //         _vaultAddress
-    //     );
-
-    //     require(vars._allowance != 0, "Allowance == 0");
-
-    //     // 2. Mint
-    //     IVAULT(_vaultAddress).mint(_collateralAmount);
-    //     success = vars.approveResponse;
-    // }
-
 }
